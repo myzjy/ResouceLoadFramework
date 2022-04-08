6374e7000e7e798b15f68d6c86df41728b0315d4
zjy <2910553808@qq.com>
2022-04-08 23:28:25 +0800

    项目第一次提交

---
+++ Assets/ResouceLoad/ResouceLoad.Script/ResouceLoadDebug.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ResouceLoadFramework.DebugLog
+{
+    /// <summary>
+    /// resouceLoad Debug
+    /// </summary>
+    public class ResouceLoadDebug : UnityEngine.Debug
+    {
+        /// <summary>
+        /// log单例 
+        /// </summary>
+        private static ResouceLoadDebug debugInstance = null;
+
+        /// <summary>
+        /// 外部调用log 打印
+        /// </summary>
+        /// <returns></returns>
+        public static ResouceLoadDebug DebugInstance()
+        {
+            if (debugInstance == null)
+            {
+                debugInstance = new ResouceLoadDebug();
+            }
+
+            //返回单例
+            return debugInstance;
+        }
+
+        /// <summary>
+        /// 输入到
+        /// </summary>
+        /// <param name="context"></param>
+        public static void Log(string context)
+        {
+            Console.WriteLine();
+        }
+    }
+}
